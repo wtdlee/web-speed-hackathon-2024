9a9bd9ad272699fbbe24fb441350752ba9833f1e
Kyuhwan Lee <kyuhwan@wantedly.com>
2024-03-24 16:49:42 +0900

    admin-book

--- workspaces/admin/src/pages/BookListPage/internal/BookDetailModal/index.tsx
+++ workspaces/admin/src/pages/BookListPage/internal/BookDetailModal/index.tsx
@@ -17,7 +17,7 @@ import {
   Tr,
 } from '@chakra-ui/react';
 import { Link } from '@tanstack/react-router';
-import { useToggle } from '@uidotdev/usehooks';
+import { useState } from 'react';
 
 import { useBook } from '../../../../features/books/hooks/useBook';
 import { useEpisodeList } from '../../../../features/episodes/hooks/useEpisodeList';
@@ -31,11 +31,11 @@ type Props = {
   onClose: () => void;
 };
 
-export const BookDetailModal: React.FC<Props> = ({ bookId, isOpen, onClose }) => {
+const BookDetailModal: React.FC<Props> = ({ bookId, isOpen, onClose }) => {
   const { data: episodeList } = useEpisodeList({ bookId });
   const { data: book } = useBook({ bookId });
 
-  const [isEdit, toggleIEdit] = useToggle(false);
+  const [isEdit, toggleIEdit] = useState(false);
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="4xl">
@@ -46,9 +46,9 @@ export const BookDetailModal: React.FC<Props> = ({ bookId, isOpen, onClose }) =>
           {book != null && (
             <>
               {isEdit ? (
-                <BookEditContent book={book} onEditComplete={() => toggleIEdit()} />
+                <BookEditContent book={book} onEditComplete={() => toggleIEdit(true)} />
               ) : (
-                <BookDetailContent book={book} onCloseDialog={onClose} onEdit={() => toggleIEdit()} />
+                <BookDetailContent book={book} onCloseDialog={onClose} onEdit={() => toggleIEdit(false)} />
               )}
             </>
           )}
@@ -118,3 +118,5 @@ export const BookDetailModal: React.FC<Props> = ({ bookId, isOpen, onClose }) =>
     </Modal>
   );
 };
+
+export default BookDetailModal;
